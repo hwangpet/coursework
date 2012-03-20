59b8757bc5285f0bc09188ad96233296dde1de37
Peter Hwang <hwangpet@gmail.com>
2012-03-20 16:20:04 -0500

    My first assignment

---
+++ week1_numbers.rb
@@ -0,0 +1,12 @@
+##Hours in year
+puts 24*365 
+
+##Minutes in a decade
+puts (60*24)*(365)*(10)
+
+##seconds old
+puts (60*60*24*365*28)
+
+## 1031 million seconds old
+puts 1031000000/60/60/24/365
+
